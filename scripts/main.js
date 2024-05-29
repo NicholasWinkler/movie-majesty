7c6a3c39f1d84f3d37405e5f129b1c7ab2de2972
Nicholas Winkler <nhwinkler@gmail.com>
2024-05-29 13:10:39 -0500

    Initial commit

--- scripts/main.js
+++
@@ -1,5 +0,0 @@
-import { generateMoviesHTML } from './movie-converter.js';
-import { renderMoviesToDOM } from './movie-render.mjs';
-
-const movieHTML = generateMoviesHTML();
-renderMoviesToDOM(movieHTML);
